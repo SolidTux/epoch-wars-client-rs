3b0c7647a7ca6cb5612717442469684b336f9f8a
Daniel Hauck <mail@dhauck.eu>
2018-09-29 12:29:57 +0200

    Better error printing.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #[macro_use]
 extern crate serde_derive;
+#[macro_use]
+extern crate failure;
 
 extern crate serde;
 extern crate serde_json;
 
+use failure::Error;
 use std::io::prelude::*;
 use std::io::{stdin, BufReader};
 use std::net::TcpStream;
@@ -18,31 +21,54 @@ enum Command {
         y: usize,
         building: String,
     },
+    Excavate {
+        x: usize,
+        y: usize,
+    },
 }
 
 impl Command {
-    pub fn from_line(s: &str) -> Option<Command> {
+    pub fn from_line(s: &str) -> Result<Command, Error> {
         let parts = s.trim().split(' ').collect::<Vec<&str>>();
         match parts.get(0) {
-            Some(&"build") => {
+            Some(&"b") => {
                 if parts.len() < 4 {
-                    None
+                    Err(format_err!("Not enough arguments."))
                 } else {
-                    Some(Command::Build {
-                        x: parts[1].parse().ok()?,
-                        y: parts[2].parse().ok()?,
+                    Ok(Command::Build {
+                        x: parts[1].parse()?,
+                        y: parts[2].parse()?,
                         building: parts[3].to_string(),
                     })
                 }
             }
-            _ => None,
+            Some(&"e") => {
+                if parts.len() < 3 {
+                    Err(format_err!("Not enough arguments."))
+                } else {
+                    Ok(Command::Excavate {
+                        x: parts[1].parse()?,
+                        y: parts[2].parse()?,
+                    })
+                }
+            }
+            Some(s) => Err(format_err!("Unknown command \"{}\".", s)),
+            None => Err(format_err!("No command specified.")),
         }
     }
 }
 
 fn main() {
-    let mut stream = TcpStream::connect("localhost:4200").unwrap();
-    let mut reader = BufReader::new(stream.try_clone().unwrap());
+    if let Err(err) = main_res() {
+        for e in err.iter_chain() {
+            eprintln!("{}", e);
+        }
+    }
+}
+
+fn main_res() -> Result<(), Error> {
+    let mut stream = TcpStream::connect("localhost:4200")?;
+    let mut reader = BufReader::new(stream.try_clone()?);
     let handle = thread::spawn(move || {
         let mut line = String::new();
         while reader.read_line(&mut line).is_ok() {
@@ -54,13 +80,23 @@ fn main() {
     let mut reader = BufReader::new(stdin());
     let mut line = String::new();
     while reader.read_line(&mut line).is_ok() {
-        if let Some(cmd) = Command::from_line(&line) {
-            let s = serde_json::to_string(&cmd).unwrap();
-            println!("{}", s);
-            writeln!(&mut stream, "{}", s);
+        match Command::from_line(&line) {
+            Ok(cmd) => {
+                let s = serde_json::to_string(&cmd)?;
+                println!("{}", s);
+                writeln!(&mut stream, "{}", s)?;
+            }
+            Err(err) => {
+                for e in err.iter_chain() {
+                    eprintln!("{}", e);
+                }
+            }
         }
         line.clear();
     }
 
-    handle.join().unwrap();
+    handle
+        .join()
+        .map_err(|_| format_err!("Error while joining thread."))?;
+    Ok(())
 }
